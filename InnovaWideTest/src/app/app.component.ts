07368325e02be6fe796420c7f52ebfd76075b30a
Ahmed Aladawy <ahmed01146322082@gmail.com>
2024-06-08 11:29:49 +0300

    add theam color

--- InnovaWideTest/src/app/app.component.ts
+++ InnovaWideTest/src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isAuth: boolean = false;
-  constructor(private cookieService: CookieService, private router: Router) {
-    if (this.cookieService.get('token'))
+
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+    private renderer: Renderer2
+  ) {
+    if (this.cookieService.get('token')) {
       this.isAuth = true;
-    else 
-    this.isAuth = false;
+    } else {
+      this.isAuth = false;
+    }
+  }
+
+  ngOnInit() {
+    const theme = this.cookieService.get('theme');
+    if (theme === 'dark') {
+      this.renderer.addClass(document.body, 'theme-dark');
+    }
   }
 
   LogOut() {
@@ -21,5 +35,16 @@ export class AppComponent {
     this.cookieService.delete('tenent');
     this.isAuth = false;
     this.router.navigate(['/login']);
+  }
+
+  toggleTheme() {
+    const body = document.body;
+    if (body.classList.contains('theme-dark')) {
+      this.renderer.removeClass(body, 'theme-dark');
+      this.cookieService.set('theme', 'light');
+    } else {
+      this.renderer.addClass(body, 'theme-dark');
+      this.cookieService.set('theme', 'dark');
+    }
   }
 }
